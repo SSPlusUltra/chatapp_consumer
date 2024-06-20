31a4fc876d9ee209e78bc4e2433ce66d249112e2
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2024-06-19 19:51:59 -0500

    cors added

--- src/main/java/com/kafkademo/chatapp_consumer/controllers/ConsumerController.java
+++ src/main/java/com/kafkademo/chatapp_consumer/controllers/ConsumerController.java
@@ -2,12 +2,14 @@ package com.kafkademo.chatapp_consumer.controllers;
 
 import com.kafkademo.chatapp_consumer.services.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/messages")
+@CrossOrigin(origins = "*")
 public class ConsumerController {
 
     @Autowired
